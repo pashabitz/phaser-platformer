ee98547d612d3e2a54b39257d2f855ad2465fe5b
Pasha Bitz <pavelbitz@gmail.com>
2025-05-09 20:29:02 -0700

    refactor to a base level scene

---
+++ src/game/scenes/base-level.js
@@ -0,0 +1,126 @@
+import { Scene } from "phaser";
+
+export class BaseLevel extends Scene {
+    constructor(key) {
+        super(key);
+        this.score = 0;
+        this.scoreText = null;
+        this.gameOver = false;
+    }
+
+    create() {
+        this.physics.world.setBounds(0, 0, 1600, 600);
+        this.cameras.main.setBounds(0, 0, 1600, 600);
+
+        this.bombs = this.physics.add.group();
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+
+        this.addBomb();
+
+        this.platforms = this.physics.add.staticGroup();
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' })
+            .setScrollFactor(0);
+
+        this.cameras.main.startFollow(this.player);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.input.on('pointerdown', this.restartGame, this);
+
+    }
+    restartGame() {
+        this.scene.start(this.key);
+        this.gameOver = false;
+    }
+
+    addBomb() {
+        var x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = this.bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        const velocityX = Phaser.Math.Between(0, 1) === 0
+            ? Phaser.Math.Between(-200, -100)
+            : Phaser.Math.Between(100, 200);
+        bomb.setVelocity(velocityX, 20);
+    }
+    collectStar(player, star) {
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+
+        this.generateNewStars();
+    }
+    generateNewStars() {
+        if (this.stars.countActive(true) === 0) {
+            //  A new batch of stars to collect
+            this.stars.children.iterate(function (child) {
+
+                child.enableBody(true, child.x, 0, true, true);
+
+            });
+
+            this.addBomb();
+        }
+    }
+    makeMovingPlatform(x, y, speed, offset) {
+        const movingPlatform = this.physics.add.image(x, y, 'ground');
+        movingPlatform.setImmovable(true);
+        movingPlatform.body.allowGravity = false;
+        movingPlatform.setVelocityX(speed); // Initial velocity
+
+        this.tweens.add({
+            targets: movingPlatform,
+            x: movingPlatform.x + offset, // Move 200 pixels to the right
+            duration: 3000, // 3 seconds
+            ease: 'Linear',
+            yoyo: true, // Go back and forth
+            repeat: -1, // Repeat indefinitely
+            onYoyo: () => { movingPlatform.setVelocityX(-speed); }, // Reverse velocity on yoyo
+            onRepeat: () => { movingPlatform.setVelocityX(speed); } // Reset velocity on repeat
+        });
+        return movingPlatform;
+    }
+
+    hitBomb(player, bomb) {
+        this.physics.pause();
+
+        player.setTint(0xff0000);
+
+        player.anims.play('turn');
+
+        this.gameOver = true;
+    }
+    hitSpikes(player, spikes) {
+        this.physics.pause();
+
+        player.setTint(0x00ff00);
+
+        player.anims.play('turn');
+
+        this.gameOver = true;
+    }
+}
--- src/game/scenes/level1.js
+++ src/game/scenes/level1.js
@@ -1,11 +1,8 @@
-import { Scene } from 'phaser';
+import { BaseLevel } from './base-level';
 
-export class Level1 extends Scene {
+export class Level1 extends BaseLevel {
     constructor() {
         super('Level1');
-        this.score = 0;
-        this.scoreText = null;
-        this.gameOver = false;
     }
 
     preload() {
@@ -23,118 +20,24 @@ export class Level1 extends Scene {
         );
 
     }
-    addBomb() {
-        var x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
-        var bomb = this.bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        const velocityX = Phaser.Math.Between(0, 1) === 0
-            ? Phaser.Math.Between(-200, -100)
-            : Phaser.Math.Between(100, 200);
-        bomb.setVelocity(velocityX, 20);
-        // bomb.allowGravity = false;
-    }
-    collectStar(player, star) {
-        star.disableBody(true, true);
-        this.score += 10;
-        this.scoreText.setText('Score: ' + this.score);
-
-        this.generateNewStars();
-    }
-    generateNewStars() {
-        if (this.stars.countActive(true) === 0) {
-            //  A new batch of stars to collect
-            this.stars.children.iterate(function (child) {
-
-                child.enableBody(true, child.x, 0, true, true);
-
-            });
-
-            this.addBomb();
-        }
-    }
-
-    hitBomb(player, bomb) {
-        this.physics.pause();
-
-        player.setTint(0xff0000);
-
-        player.anims.play('turn');
-
-        this.gameOver = true;
-    }
-
-    hitSpikes(player, spikes) {
-        this.physics.pause();
-
-        player.setTint(0x00ff00);
-
-        player.anims.play('turn');
-
-        this.gameOver = true;
-    }
+    
     create() {
+        super.create();
 
-        this.physics.world.setBounds(0, 0, 1600, 600);
-        this.cameras.main.setBounds(0, 0, 1600, 600);
-        const platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(6, 2).refreshBody();
 
-        platforms.create(400, 568, 'ground').setScale(6, 2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(1200, 320, 'ground');
 
-        platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        // platforms.create(750, 180, 'ground');
-        platforms.create(1200, 320, 'ground');
+        const movingPlatform = this.makeMovingPlatform(750, 180, 50, 200);
 
-        // Create a moving platform
-        const movingPlatform = this.physics.add.image(750, 180, 'ground');
-        movingPlatform.setImmovable(true);
-        movingPlatform.body.allowGravity = false;
-        movingPlatform.setVelocityX(50); // Initial velocity
-
-        this.tweens.add({
-            targets: movingPlatform,
-            x: movingPlatform.x + 200, // Move 200 pixels to the right
-            duration: 3000, // 3 seconds
-            ease: 'Linear',
-            yoyo: true, // Go back and forth
-            repeat: -1, // Repeat indefinitely
-            onYoyo: () => { movingPlatform.setVelocityX(-50); }, // Reverse velocity on yoyo
-            onRepeat: () => { movingPlatform.setVelocityX(50); } // Reset velocity on repeat
-        });
+        
 
         const spikes = this.physics.add.staticGroup();
         spikes.create(600, 366, 'spikes').setDepth(-1);
 
-
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.cameras.main.startFollow(this.player);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.cursors = this.input.keyboard.createCursorKeys();
 
 
         this.stars = this.physics.add.group({
@@ -144,39 +47,26 @@ export class Level1 extends Scene {
         });
 
         this.stars.children.iterate(function (child) {
-
             child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
-
         });
 
-
-        this.bombs = this.physics.add.group();
-        this.addBomb();
-
-        this.physics.add.collider(this.bombs, platforms);
+        this.physics.add.collider(this.bombs, this.platforms);
 
 
-        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' })
-            .setScrollFactor(0);
-
-        this.physics.add.collider(this.player, platforms);
+        this.physics.add.collider(this.player, this.platforms);
         this.physics.add.collider(this.player, movingPlatform);
-        this.physics.add.collider(this.stars, platforms);
+        this.physics.add.collider(this.stars, this.platforms);
         this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
         this.physics.add.overlap(spikes, this.stars, (spikes, star) => {
             star.disableBody(true, true);
         }, null, this);
         this.physics.add.collider(this.stars, movingPlatform);
-        this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
         this.physics.add.collider(this.player, spikes, this.hitSpikes, null, this);
 
-        this.input.on('pointerdown', this.restartGame, this);
     }
 
-    restartGame() {
-        this.scene.start('Level1');
-        this.gameOver = false;
-    }
+
+
 
 
     update() {
