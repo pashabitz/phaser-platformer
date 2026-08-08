bcfb1959f9051cf91cde16dd30fb287f770825e2
Pasha Bitz <pavelbitz@gmail.com>
2026-08-08 16:06:10 -0700

    Clean up level5

--- src/game/scenes/level5.js
+++ src/game/scenes/level5.js
@@ -12,15 +12,15 @@ export class Level5 extends BaseLevel {
 
     create() {
         super.create({
+            platforms: [
+                { x: 0, y: 600, scaleX: 8 },
+                { x: 1120, y: 400 }
+            ],
             spikes: [
                 { x: 1020, y: 570 },
                 { x: 1220, y: 570 }
             ]
         });
-
-
-        this.platforms.create(0, 600, 'ground').setScale(8, 1).refreshBody();
-        this.platforms.create(1120, 400, 'ground').setScale(1, 1).refreshBody();
 
         this.lasers = [600, 900, 1400].map((x) => (
             new Laser(this, this.player).create(x, 25)
